e45aaa44f71f66babbf4b112e2d07694a857eca8
Seth Kwame Addo <addoseth7@gmail.com>
2023-07-26 19:15:32 +0100

    got the front end to link the backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+const [searchQuery, setsearchQuery] = useState("")
+const [image, setimage] = useState("")
+
+const handleSearch = (event) =>{
+  setsearchQuery(event.target.value)
+  // console.log(searchQuery)
+}
+
+  const handleImage = async ()=>{
+    try{
+  let randomNumber = Math.floor(Math.random()*10)
+  console.log(randomNumber)
+  const API = `http://localhost:8090/photos?subject=${searchQuery}`
+  const res = await axios.get(API)
+  setimage(res.data[randomNumber].img_url)
+  console.log(res)
+}catch(error){
+  console.log(error)
+}
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>Find your image</h1>
+  <input type='text'placeholder='enter image subject' onChange={handleSearch}></input>
+  <button onClick={handleImage}>Explore!</button>
+  { image && <img src= {image} alt={searchQuery}></img>}
     </div>
   );
 }
